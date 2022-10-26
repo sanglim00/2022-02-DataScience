816f626226f79540012adb8b00c4533e19542d72
Sanglim <54923245+sanglim00@users.noreply.github.com>
2022-10-26 11:12:56 +0900

    Update 2021 01 과제.py

--- 2021 report/2021 01 과제.py
+++ 2021 report/2021 01 과제.py
@@ -1,17 +1,13 @@
 class Matrix:
   def __init__(self, data):
     self.data = data
-    
   def __str__(self):
     return f"Matrix({self.data})"
-    
   def __add__(self, another):
     if len(self.data) == 0 or len(another.data) == 0:
       raise Exception("Assertion Error")
-      
     elif len(self.data) != len(another.data):
       raise Exception("Assertion Error")
-      
     elif len(self.data[0]) != len(another.data[0]):
       raise Exception("Assertion Error")
 
@@ -26,10 +22,8 @@ class Matrix:
   def __sub__(self, another):
     if len(self.data) == 0 or len(another.data) == 0:
       raise Exception("Assertion Error")
-      
     elif len(self.data) != len(another.data):
       raise Exception("Assertion Error")
-      
     elif len(self.data[0]) != len(another.data[0]):
       raise Exception("Assertion Error")
     
@@ -41,57 +35,37 @@ class Matrix:
 
     return Matrix(ret)
 
-  def __mul__(self, another):
-    ret = [[0 for x in self.data[0]] for y in self.data]
+  def __mul__(self, other):
+    if type(other) == Matrix:
+        assert len(self.data[0]) == len(other.data)
 
-    if(type(another) == int):
-      for i in range(0, len(self.data)):
-        for j in range(0, len(self.data[0])):
-          ret[i][j] = self.data[i][j] * another
+        tmp = [[0] * len(other.data[0]) for i in range(len(self.data))]
+        for i in range(len(self.data)):
+            for k in range(len(other.data[0])):
+                total = 0
+                for j in range(len(other.data)):
+                    total += self.data[i][j] * other.data[j][k]
+                tmp[i][k] = total
+        return Matrix(tmp)
+
     else:
-      if len(self.data) == 0 or len(another.data) == 0:
-        raise Exception("Assertion Error")
-        
-      elif len(self.data) != len(another.data):
-        raise Exception("Assertion Error")
-        
-      elif len(self.data[0]) != len(another.data[0]):
-        raise Exception("Assertion Error")
-      
-      for i in range(0, len(self.data)):
-        for j in range(0, len(self.data[0])):
-          ret[i][j] = self.data[i][j] * another.data[i][j]
+        tmp = [[0] * len(self.data[0]) for i in range(len(self.data))]
+        for i in range(len(self.data)):
+            for j in range(len(self.data[0])):
+                tmp[i][j] = self.data[i][j]*other
+        return Matrix(tmp)
 
-    return Matrix(ret)
-
-  def __rmul__(self, another):
-    ret = [[0 for x in self.data[0]] for y in self.data]
-    
-    if(type(another) == int):
-      for i in range(0, len(self.data)):
-        for j in range(0, len(self.data[0])):
-          ret[i][j] = self.data[i][j] * another
-          
-    else:
-      if len(self.data) == 0 or len(another.data) == 0:
-        raise Exception("Assertion Error")
-        
-      elif len(self.data) != len(another.data):
-        raise Exception("Assertion Error")
-        
-      elif len(self.data[0]) != len(another.data[0]):
-        raise Exception("Assertion Error")
-
-      for i in range(0, len(self.data)):
-        for j in range(0, len(self.data[0])):
-          ret[i][j] = self.data[i][j] * another.data[i][j]
-
-    return Matrix(ret)
+  def __rmul__(self, other):
+    tmp = [[0] * len(self.data[0]) for i in range(len(self.data))]
+    for i in range(len(self.data)):
+        for j in range(len(self.data[0])):
+            tmp[i][j] = self.data[i][j] * other
+    return Matrix(tmp)
 
 a = Matrix([[1,2,3], [4,5,6]])
-b = Matrix([[1,2,3], [4,5,6]])
-print(a + b)
-print(a - b)
-print(a * 2)
-print(2 * a)
+b = Matrix([[1,2], [4,5], [6,7]])
+# print(a + b)
+# print(a - b)
+# print(a * 2)
+# print(2 * a)
 print(a * b)
